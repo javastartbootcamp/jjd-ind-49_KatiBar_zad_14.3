19aca5279da549c09551b3b1345b7302561dc70b
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-01-08 01:00:06 +0100

    Dodanie logiki w klasie Main, klasy Country oraz pliku "countries.csv"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Main {
 
@@ -10,7 +12,39 @@ public class Main {
     }
 
     void run(Scanner scanner) {
-        // usupełnij metodę
+        String fileName = "countries.csv";
+        try {
+            Map<String, Country> countriesMap = readFileAndGetCountriesMap(fileName);
+            System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje:");
+            String codeFromUser = scanner.nextLine();
+            checkOrContainsCountry(countriesMap, codeFromUser);
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku " + fileName + ".");
+        }
+    }
+
+    private static void checkOrContainsCountry(Map<String, Country> countriesMap, String codeFromUser) {
+        if (countriesMap.containsKey(codeFromUser)) {
+            System.out.println(countriesMap.get(codeFromUser));
+        } else {
+            System.out.println("Kod kraju " + codeFromUser + " nie został znaleziony.");
+        }
+    }
+
+    private Map<String, Country> readFileAndGetCountriesMap(String fileName) throws FileNotFoundException {
+        Map<String, Country> countryMap = new HashMap<>();
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] split = line.split(";");
+                String code = split[0];
+                String name = split[1];
+                int population = Integer.parseInt(split[2]);
+                Country country = new Country(code, name, population);
+                countryMap.put(code, country);
+            }
+        }
+        return countryMap;
     }
 
 }
